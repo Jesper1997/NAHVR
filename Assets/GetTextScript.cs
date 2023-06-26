7ccadc37a62694acc106f2b49578330fa90c40f1
ChickenSorcerer <casperhermans2000@gmail.com>
2023-06-26 17:46:44 +0200

    Phone
    The phone now rings if the player has cleared certain tasks and when the player picks the phone of the countertop the call starts.

--- Assets/GetTextScript.cs
+++ Assets/GetTextScript.cs
@@ -1,13 +1,22 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine;
 
 public class GetTextScript : MonoBehaviour
 {
     public GameObject Todolist;
     private ToDoListing ToDoListing;
-    public int index;
+    public int todolist_index1 = 1;
+    public int todolist_index2 = 2;
+    public AudioClip ringtone;
+    public AudioClip phonecallaudio1;
+    public AudioClip phonecallaudio2;
+    public AudioSource phoneSpeaker;
+    bool phonecall1 = false;
+    bool phonecall2 = false;
+    int phonecall = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +26,43 @@ public class GetTextScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (ToDoListing.list[index].Description.Contains("<s>"))
+        if ((ToDoListing.list[todolist_index1].Description.Contains("<s>")) && (phonecall != 1))
         {
             //Execution methode
-            Debug.Log(ToDoListing.list[index].Description);
+            Debug.Log(ToDoListing.list[todolist_index1].Description);
+            phoneSpeaker.loop = true;
+            phonecall = 1;
+            phoneSpeaker.clip = ringtone;
+            phoneSpeaker.Play();
         }
 
+        if ((ToDoListing.list[todolist_index2].Description.Contains("<s>")) && (phonecall != 2))
+        {
+            //Execution methode
+            Debug.Log(ToDoListing.list[todolist_index2].Description);
+            phoneSpeaker.loop = true;
+            phonecall = 2;
+            phoneSpeaker.clip = ringtone;
+            phoneSpeaker.Play();
+        }
     }
 
-
+    public void Playmessage()
+    {
+        if ((phonecall == 1) && (!phonecall1))
+        {
+            phoneSpeaker.clip = phonecallaudio1;
+            phoneSpeaker.loop = false;
+            phoneSpeaker.Play();
+            phonecall1 = true;
+        }
+        if ((phonecall == 2) && (!phonecall2))
+        {
+            phoneSpeaker.clip = phonecallaudio2;
+            phoneSpeaker.loop = false;
+            phoneSpeaker.Play();
+            phonecall2 = true;
+        }
+    }
 }
+
